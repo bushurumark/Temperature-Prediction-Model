27afd40a4f5704b27b036335ee3d9af0dcdc2243
Levi Mark Bushuru <120096080+bushurumark@users.noreply.github.com>
2024-06-07 15:59:02 +0300

    Add files via upload

--- App.py
+++ App.py
@@ -21,6 +21,7 @@ model = joblib.load("model.pkl")
 def predict_temperature(features):
     return model.predict([features])[0]
 
+# Streamlit app
 st.markdown(
     """
     <style>
@@ -42,9 +43,6 @@ st.markdown(
         font-size: 24px;
         margin-top: 20px;
     }
-    body {
-        background-color: blue; 
-    }
     </style>
     """, unsafe_allow_html=True)
 
@@ -55,17 +53,15 @@ energy_delta = st.number_input("Energy delta[Wh]", value=0.0)
 ghi = st.number_input("Global Horizontal Irradiance", value=0.0)
 pressure = st.number_input("Pressure", value=1013.25)
 humidity = st.number_input("Humidity", value=50.0)
-wind_speed = st.number_input("Wind speed", value=0.0)
+wind_speed = st.number_input("Wind Speed", value=0.0)
 rain_1h = st.number_input("Rain", value=0.0)
 snow_1h = st.number_input("Snow", value=0.0)
 clouds_all = st.number_input("Clouds", value=0.0)
 is_sun = st.selectbox("Availability of Sun", [0, 1])
 sunlight_time = st.number_input("Sun Light Time", value=0.0)
 day_length = st.number_input("Day Length", value=0.0)
-sunlight_ratio = st.number_input("Sunlight Time/Day Length", value=0.0)
-Type_levels = {1: 'Clear', 2: 'Clouds', 3: 'Rain', 4: 'Snow', 5: 'Other'}
-options = [f"{key}-{value}" for key, value in Type_levels.items()]
-weather_type = st.selectbox('Weather Type', options)
+sunlight_ratio = st.number_input("Sunlight Time/day length", value=0.0)
+weather_type = st.selectbox("Weather Type", [1, 2, 3, 4, 5])
 hour = st.number_input("Hour", min_value=0, max_value=23, value=0)
 month = st.number_input("Month", min_value=1, max_value=12, value=1)
 
